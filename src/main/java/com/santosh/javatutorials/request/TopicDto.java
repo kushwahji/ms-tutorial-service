41291f1c4380d8b2dfb7a83eec93eae38ac473b9
SANTOSH <31657114+kushwahji@users.noreply.github.com>
2022-02-21 14:14:13 +0530

    Add files via upload

--- src/main/java/com/santosh/javatutorials/request/TopicDto.java
+++ src/main/java/com/santosh/javatutorials/request/TopicDto.java
@@ -1,21 +1,19 @@
 package com.santosh.javatutorials.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.santosh.javatutorials.entity.Topic;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
-import java.util.List;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class TopicDto {
+
+    @NotBlank(message = "name should not be blank or null")
     @NotNull(message = "name should not be blank or null")
     private String name;
 
